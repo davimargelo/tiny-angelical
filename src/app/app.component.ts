7ccc787fe020cbe159d292bbc134b0f23f6024ff
davimargelo <davimargelo@hotmail.com>
2021-07-23 17:38:42 -0300

    footer and stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +11,31 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent implements OnInit {
   title = 'tiny-angelical';
-  images = ['.\\assets\\img\\product_1.png', '.\\assets\\img\\product_2.png', '.\\assets\\img\\product_3.png'];
+  freteImg = '.\\assets\\img\\fretegratis.png'
+  images = [
+    { path: '.\\assets\\img\\product_1.png', title: 'Pijaminha kawaii', description: 'Já deu uma passadinha na nossa seção kawaii? Corre lá e surpreenda-se! Muitas novidades e preços incríveis.' },
+    { path: '.\\assets\\img\\product_2.png', title: 'Brincos da vitalidade + Brinde Choker', description: 'Promoção: Leve 2, pague 1! Na compra de um par de brincos ganhe um choker combinando!' },
+    { path: '.\\assets\\img\\product_3.png', title: 'Manto Demonslayer Feminino', description: 'Nossa loja tem uma seção exclusiva de roupas para Cosplayers, com tendências sempre em alta e preços super em baixa. Aproveite.' }
+  ];
   logoImg = '.\\assets\\img\\tinyangelicallogo.png'
-  options: string[] = ['Início', 'Categorias', 'Contato', 'Temas']
+  options: any[] = [{ title: 'Início', link: '/' }, { title: 'Categorias', link: null }, { title: 'Contato', link: 'contato' }, { title: 'Temas', link: 'temas' }]
   paintSearchIcon: boolean = false;
 
-  constructor(private config: NgbCarouselConfig) {
+  constructor(private config: NgbCarouselConfig, private router: Router, private route: ActivatedRoute, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
     // customize default values of carousels used by this component tree
     this.config.interval = 10000;
     this.config.wrap = false;
     this.config.keyboard = false;
     this.config.pauseOnHover = true;
+
+    this.matIconRegistry.addSvgIcon(
+      `instagram`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/instagram-logo.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      `twitter`,
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/svg/twitter-logo.svg")
+    );
   }
 
   ngOnInit(): void {
@@ -30,5 +47,10 @@ export class AppComponent implements OnInit {
 
   searchOnBlur() {
     this.paintSearchIcon = false;
+  }
+
+  clickedOption(option: any): void {
+    if (option.link !== null)
+      this.router.navigate([option.link], { relativeTo: this.route });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     MaterialModule,
     FlexLayoutModule,
     NgbModule,
+    HttpClientModule,
     ReactiveFormsModule
   ],
   providers: [],
